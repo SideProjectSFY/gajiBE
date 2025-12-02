9ff126b98b3420a4edad637f9d2edd09e956db32
yeomin4242 <whereismysejong@naver.com>
2025-12-02 09:19:56 +0900

    feat: Add ScenarioType enum for representing different types of scenario changes

---
+++ backend/src/main/java/com/gaji/corebackend/enums/ScenarioType.java
@@ -0,0 +1,11 @@
+package com.gaji.corebackend.enums;
+
+/**
+ * Scenario Type Enum
+ * Represents the type of scenario change
+ */
+public enum ScenarioType {
+    CHARACTER_CHANGE,
+    EVENT_ALTERATION,
+    SETTING_MODIFICATION
+}
